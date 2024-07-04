140fd75ff46b50cce7dd8ad3300307773261d4c2
Hhy <hsuihung@163.com>
2024-07-04 20:12:42 +0800

    策略权重装配

---
+++ marketing-domain/src/main/java/cn/hhy/domain/strategy/service/armory/IStrategyDispatch.java
@@ -0,0 +1,20 @@
+package cn.hhy.domain.strategy.service.armory;
+
+/**
+ * @author Hhy
+ * @description 策略抽奖调度
+ * @create 2024/7/4
+ */
+public interface IStrategyDispatch {
+
+    /**
+     * 获取抽奖策略装配的随机结果
+     *
+     * @param strategyId 策略ID
+     * @return 抽奖结果
+     */
+    Integer getRandomAwardId(Long strategyId);
+
+    Integer getRandomAwardId(Long strategyId, String ruleWeightValue);
+
+}
