690039040948258c2774132543dda1c8762e3893
Hhy <hsuihung@163.com>
2024-07-11 20:15:16 +0800

    第一阶段部署

--- marketing-app/build.sh
+++ marketing-app/build.sh
@@ -1,5 +1,5 @@
 # 普通镜像构建，随系统版本构建 amd/arm
-docker build -t thehhy/marketing-app:1.0-SNAPSHOT -f ./Dockerfile .
+#docker build -t thehhy/marketing-app:1.0-SNAPSHOT -f ./Dockerfile .
 
-# 兼容 amd、arm 构建镜像
-# docker buildx build --load --platform liunx/amd64,linux/arm64 -t xiaofuge/xfg-frame-archetype-app:1.0 -f ./Dockerfile . --push
+# mac构建amd版本docker
+docker buildx build --push --platform liunx/amd64 -t thehhy/marketing-app:1.0-SNAPSHOT -f ./Dockerfile .
