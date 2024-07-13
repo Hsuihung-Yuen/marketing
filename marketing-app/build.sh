8238af4cde09b6616782a5d322309042429bdac7
Hhy <hsuihung@163.com>
2024-07-13 20:21:19 +0800

    第一阶段部署

--- marketing-app/build.sh
+++ marketing-app/build.sh
@@ -1,5 +1,5 @@
 # 普通镜像构建，随系统版本构建 amd/arm
-docker build -t thehhy/marketing-app:1.0 -f ./Dockerfile . --push
+docker build -t thehhy/marketing-app:1.0 -f ./Dockerfile .
 
 # mac构建amd版本docker
 #docker buildx build --push --platform liunx/arm64 -t thehhy/marketing-app:1.0-SNAPSHOT .
